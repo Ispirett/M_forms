5fc4480135d5608604bd6fa6fd7995505c3ab47e
Macatuz <armando29@live.com>
2019-06-13 20:38:26 -0400

    Editing 5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     end
 
     def edit
-
+        @user = User.find(params[:id])
     end
 
     def update
@@ -26,7 +26,9 @@ class UsersController < ApplicationController
      end
 
     end
-
+    def show
+        @user = User.find(params[:id])
+    end
     private
     def user_params
       params.require(:user).permit(:name, :email, :password)
